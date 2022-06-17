7dd2ef7db6763f588821209f75725c829cbf47ca
AhmetTpc <ahmttpc14@gmail.com>
2022-06-17 23:09:38 +0300

    First Commit

--- src/main/java/com/gokhantamkoc/javabootcamp/odevhafta2/PlayWorldOfMagic.java
+++ src/main/java/com/gokhantamkoc/javabootcamp/odevhafta2/PlayWorldOfMagic.java
@@ -12,19 +12,18 @@ public class PlayWorldOfMagic implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
 		int maxNumOfAttacksAllowed = 9;
 		String[] magicianSpells = createSpellNameRepository();
 		float[] spellDamageInfo = createSpellDamageRepository();
 		String[] bossNames = createBossNameRepository();
 		float[] bossHps = createBossHPRepository();
-		
+
 		int minNumberSpellsUsed = resolveBattle(
-				magicianSpells, 
-				spellDamageInfo, 
-				bossNames, 
+				magicianSpells,
+				spellDamageInfo,
+				bossNames,
 				bossHps);
-		
+
 		if (minNumberSpellsUsed > maxNumOfAttacksAllowed) {
 			System.out.println("Magician died!");
 		} else if (minNumberSpellsUsed > 0 && minNumberSpellsUsed <= maxNumOfAttacksAllowed) {
@@ -33,37 +32,67 @@ public class PlayWorldOfMagic implements CommandLineRunner {
 			System.out.println("Result is not correct!");
 		}
 	}
-	
+
 	public static int resolveBattle(
 			String[] magicianSpells,
 			float[] spellDamageInfo,
 			String[] bossNames,
-			float[] bossHPs
-			) {
-		
+			float[] bossHPs) {
+
 		int spellsUsed = 0;
 		// ______ BASLANGIC _______ Kodunuz buradan baslamali
-		
+		String firstBoss = bossNames[0];
+		float firstBossHp = bossHPs[0];
+		String secondBoss = bossNames[1];
+		float secondBossHp = bossHPs[1];
+		String thirdBoss = bossNames[2];
+		float thirdBossHp = bossHPs[2];
+
+		float SelectedSpell = spellDamageInfo[0];
+		for (int i = 1; i < spellDamageInfo.length; i++) {
+			if (spellDamageInfo[i] > SelectedSpell) {
+				SelectedSpell = spellDamageInfo[i];
+			}
+		}
+		while (true) {
+
+			if (firstBossHp > 0) {
+				firstBossHp = firstBossHp - SelectedSpell;
+				spellsUsed++;
+			} else if (secondBossHp > 0) {
+				secondBossHp = secondBossHp - SelectedSpell;
+				spellsUsed++;
+
+			} else if (thirdBossHp > 0) {
+				thirdBossHp = thirdBossHp - SelectedSpell;
+				spellsUsed++;
+
+				break;
+			}
+
+		}
+
 		// ______ SON _______ Kodunuz burada bitmeli
-		/* NOT: ______ BASLANGIC _______ ve ______ SON _______ 
+		/*
+		 * NOT: ______ BASLANGIC _______ ve ______ SON _______
 		 * arasina istediginiz kadar sayida satir ekleyebilirsiniz.
 		 */
 		return spellsUsed;
 	}
-	
+
 	public static String[] createSpellNameRepository() {
-		return new String[]{"Ice Storm", "Chain Lightning", "Magic Missile"};
+		return new String[] { "Ice Storm", "Chain Lightning", "Magic Missile" };
 	}
-	
+
 	public static float[] createSpellDamageRepository() {
-		return new float[]{40.0f, 30.0f, 5.0f};
+		return new float[] { 40.0f, 30.0f, 5.0f };
 	}
-	
+
 	public static String[] createBossNameRepository() {
-		return new String[]{"Dire Rat", "Skeleton Knight", "Undead King"};
+		return new String[] { "Dire Rat", "Skeleton Knight", "Undead King" };
 	}
-	
+
 	public static float[] createBossHPRepository() {
-		return new float[]{15.0f, 45.0f, 60.0f};
+		return new float[] { 15.0f, 45.0f, 60.0f };
 	}
 }
